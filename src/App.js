b9c4069a359acbe04ee769328e11ab369d5fddc1
qasimgit <qasimpkkhi@gmail.com>
2020-07-27 11:20:49 +0500

    job component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import useFetchJobs from "./api/useFetchjobs";
+import { Container } from "react-bootstrap";
+import {useState} from 'react'
+
+import {Job} from './components/jobs'
 
 function App() {
+
+  const [ params , setParams ] = useState({})
+  const { jobs, loading, error } = useFetchJobs(params );
+  
+
   return (
-    <div>
-      Muhammad Qasim Qadri
-    </div>
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error....</h1>}
+      {jobs.map(job => {
+        return <Job key={job.id} job={job}/> 
+      })
+      };
+      </Container>
   );
 }
 
---
+++ src/components/jobs.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Card} from 'react-bootstrap';
+
+export const Job = ( { job }) => {
+    return (
+        <Card>
+            <Card.Body>
+
+
+            </Card.Body>
+            {job.title}
+        </Card>
+       
+    )
+}
