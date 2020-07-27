7b2e6d7a7f0cbb57b2b121fbeda918a61d15409a
qasimgit <qasimpkkhi@gmail.com>
2020-07-27 13:18:32 +0500

    company details added

--- src/components/jobs.jsx
+++ src/components/jobs.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
-import {Card} from 'react-bootstrap';
-
-export const Job = ( { job }) => {
-    return (
-        <Card>
-            <Card.Body>
-
-
-            </Card.Body>
-            {job.title}
-        </Card>
-       
-    )
-}
+import React from "react";
+import { Card, Badge } from "react-bootstrap";
+import ReactMarkdown from "react-markdown";
+export const Job = ({ job }) => {
+  return (
+    <Card>
+      <Card.Body>
+        <div className="d-flex justify-content-between">
+          <div>
+            <Card.Title>
+              {job.title} -{"   "}
+              <span className="text-muted font-weight-light">
+                {" "}
+                {job.company}
+              </span>
+            </Card.Title>
+            <Card.Subtitle className="text-muted mb-2">
+              {new Date(job.created_at).toLocaleDateString()}
+            </Card.Subtitle>
+            <Badge variant="secondary" className="mr-2">
+              {job.type}
+            </Badge>
+            <Badge variant="secondary">{job.location}</Badge>
+            <div style={{ wordBreak: "Break-all" }}>
+              <ReactMarkdown source={job.how_to_apply} />
+            </div>
+          </div>
+        <img className='d-none d-md-block' height='50' src={job.company_logo} alt={job.company}/>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
